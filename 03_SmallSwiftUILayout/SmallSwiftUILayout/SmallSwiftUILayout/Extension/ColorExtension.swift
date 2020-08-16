74c9c19f509e2bf05758766ef96ea12ac35fcbb7
Fumiya Sakai <just1factory@gmail.com>
2020-08-16 09:14:33 +0900

    [03_SmallSwiftUILayout] プロジェクトの新規追加

---
+++ 03_SmallSwiftUILayout/SmallSwiftUILayout/SmallSwiftUILayout/Extension/ColorExtension.swift
@@ -0,0 +1,21 @@
+//
+//  ColorExtension.swift
+//  SmallSwiftUILayout
+//
+//  Created by 酒井文也 on 2020/08/16.
+//  Copyright © 2020 酒井文也. All rights reserved.
+//
+
+import SwiftUI
+
+// MEMO: 下記Stackoverflowの内容を参照
+// https://stackoverflow.com/questions/56874133/use-hex-color-in-swiftui
+
+extension Color {
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex & 0xff0000) >> 16) / 255.0
+        let green = Double((hex & 0xff00) >> 8) / 255.0
+        let blue = Double((hex & 0xff) >> 0) / 255.0
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
---
+++ 03_SmallSwiftUILayout/SmallSwiftUILayout/SmallSwiftUILayout/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmallSwiftUILayout
+//
+//  Created by 酒井文也 on 2020/08/16.
+//  Copyright © 2020 酒井文也. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
