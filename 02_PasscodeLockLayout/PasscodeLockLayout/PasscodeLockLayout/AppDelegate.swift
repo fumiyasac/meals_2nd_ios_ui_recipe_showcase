32a4b2c337b533c180e749e7b88a45aa0a35e95e
Fumiya Sakai <just1factory@gmail.com>
2022-07-16 23:00:05 +0900

    第2章: AppDelegateへの追記対応

--- 02_PasscodeLockLayout/PasscodeLockLayout/PasscodeLockLayout/AppDelegate.swift
+++ 02_PasscodeLockLayout/PasscodeLockLayout/PasscodeLockLayout/AppDelegate.swift
@@ -13,6 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("didFinishLaunchingWithOptions: アプリ起動時")
+
+        // MEMO: iOS15に関する配色に関する調整対応
+        setupNavigationBarAppearance()
+
         return true
     }
 
@@ -23,4 +27,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+
+    // MARK: - Private Function
+
+    private func setupNavigationBarAppearance() {
+
+        // iOS15以降ではUINavigationBarの配色指定方法が変化する点に注意する
+        // https://shtnkgm.com/blog/2021-08-18-ios15.html
+        if #available(iOS 15.0, *) {
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+
+            // MEMO: UINavigationBar内部におけるタイトル文字の装飾設定
+            navigationBarAppearance.titleTextAttributes = [
+                NSAttributedString.Key.font : UIFont(name: "HelveticaNeue-Bold", size: 14.0)!,
+                NSAttributedString.Key.foregroundColor : UIColor.white
+            ]
+
+            // MEMO: UINavigationBar背景色の装飾設定
+            navigationBarAppearance.backgroundColor = UIColor(code: "#ef93b6")
+
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
+    }
 }
