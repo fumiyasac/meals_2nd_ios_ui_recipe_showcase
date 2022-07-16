f85fe32e93b581bf4d3e91dcb1dc018f0fec7a8e
Fumiya Sakai <just1factory@gmail.com>
2022-07-16 23:11:17 +0900

    第3章: AppDelegateへの追記対応

--- 03_SmallSwiftUILayout/SmallSwiftUILayout/SmallSwiftUILayout/AppDelegate.swift
+++ 03_SmallSwiftUILayout/SmallSwiftUILayout/SmallSwiftUILayout/AppDelegate.swift
@@ -12,6 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        // MEMO: iOS15に関する配色に関する調整対応
+        setupNavigationBarAppearance()
+
         return true
     }
 
@@ -22,4 +26,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
+
+    // MARK: - Private Function
+
+    private func setupNavigationBarAppearance() {
+
+        // iOS15以降ではUINavigationBarの配色指定方法が変化する点に注意する
+        // https://shtnkgm.com/blog/2021-08-18-ios15.html
+        if #available(iOS 15.0, *) {
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+
+            // MEMO: UINavigationBar内部におけるタイトル文字の装飾設定
+            navigationBarAppearance.titleTextAttributes = [
+                NSAttributedString.Key.foregroundColor : UIColor.black
+            ]
+
+            // MEMO: UINavigationBar背景色の装飾設定
+            navigationBarAppearance.backgroundColor = UIColor.white
+
+            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
+    }
 }
